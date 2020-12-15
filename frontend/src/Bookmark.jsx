920e1df49f26c45477e69a7b0b965bf1ad813738
michaelmoreno <michaelmoreno.dev@gmail.com>
2020-12-14 19:31:02 -0600

    push

--- frontend/src/Bookmark.jsx
+++ frontend/src/Bookmark.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DataLayer from './DataLayer'
 
-export default function Bookmark(props) {
+export default function Bookmark({ data, api }) {
+  // state hooks
+  const [ name, setName ] = useState(data.name);
+  const [ url, setUrl ] = useState(data.url);
+
+  // user edited name text box
+  const onNameChange = event => {
+    const newName = event.target.value;
+    // use name state hook
+    setName(newName);
+  };
+
+  // user edited url text box
+  const onUrlChange = event => {
+    const newUrl = event.target.value;
+    // use url state hook
+    setUrl(newUrl);
+  };
+
+  // user pressed edit button
+  const onEdit = event =>  {
+    event.preventDefault();
+    // send new data to backend api
+    api.update(data.id, name, url);
+  };
+
+  // user pressed delete button
+  const onDelete = (event) => {
+    event.preventDefault();
+    // send deleted id to backend api
+    api.delete(data.id);
+  }
+
   return (
     <form className="bookmark">
-      <input type="text" value={props.Bookmark}/>
-      <input type="text" value={props.DataLayer}/>
-      <button type="submit"></button>
+      <input type="text" onChange={onNameChange} value={name}/>
+      <input type="text" onChange={onUrlChange} value={url}/>
+      <button onClick={onEdit}>Edit</button>
+      <button onClick={onDelete}>Delete</button>
     </form>
   )
 }
